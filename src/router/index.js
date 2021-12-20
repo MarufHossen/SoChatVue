927b3d9e77e5389f7bc2e5b6ca2413cfa1ae3c6e
basil george <baezl4life@gmail.com>
2021-12-20 02:10:36 +0200

    updated 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,8 @@
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
 import Homepage from "../views/Homepage.vue";
-<<<<<<< HEAD
 import Integration from "./../views/Integration.vue";
 import Analytics from "./../views/Analytics.vue";
-=======
-import ChatDashboard from "../components/ChatDashboard.vue";
->>>>>>> 7ceaf0c51b6163ac368ea7f24cfb0c48a6ccb9ff
 
 import { createRouter, createWebHistory } from "vue-router";
 import { watchEffect, ref } from "vue";
@@ -18,21 +14,14 @@ watchEffect(user, () => {
 });
 
 const routes = [
-<<<<<<< HEAD
   { path: "/signup", component: user ? Homepage : Signup },
   { path: "/login", component: user ? Homepage : Login },
   { path: "/", name: "home", component: !user ? Login : Homepage },
   { path: "/integration", component: user ? Integration : Login },
   { path: "/analytics", component: user ? Analytics : Login },
-=======
-    { path: "/signup", component: Signup },
-    { path: "/login", component: Login },
-    { path: "/", component: Homepage },
-    { path: "/chatdashboard", component: ChatDashboard }
->>>>>>> 7ceaf0c51b6163ac368ea7f24cfb0c48a6ccb9ff
 ];
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 export default router;
