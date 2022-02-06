91812dd806f077ce1f9d6d40c03a48e9a2e14c33
Ahmed Madani <ahmedmoeen87@gmail.com>
2022-02-06 23:07:05 +0200

    signup page login page added. Layout page script added. firebase added.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import router from "./router";
+import { initializeApp } from "firebase/app";
 
-createApp(App).use(router).mount("#app");
+const firebaseConfig = {
+    apiKey: "AIzaSyCsd2auDToUd0l0OLkwHJ6bQ9sPUioQAcE",
+    authDomain: "so-chat-vue.firebaseapp.com",
+    projectId: "so-chat-vue",
+    storageBucket: "so-chat-vue.appspot.com",
+    messagingSenderId: "574230030392",
+    appId: "1:574230030392:web:45d86377c239c08bba5ecd"
+  };
+
+  initializeApp(firebaseConfig);
+
+  const app = createApp(App)
+  app.use(router)
+  app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
 import Layout from "../views/Layout.vue";
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
 import { createRouter, createWebHistory } from "vue-router";
 import { watchEffect, ref } from "vue";
 const user = localStorage.getItem("user");
@@ -14,10 +17,41 @@ watchEffect(user, () => {
 const routes = [
   { path: "/signup", component: Signup },
   { path: "/login", component: Login },
-  { path: "/", component: Layout },
+  { path: "/", component: Layout,
+      meta: {
+        requiresAuth: true,
+        },
+    },
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) =>{
+      const removeListener = onAuthStateChanged(
+          getAuth(),
+          (user) => {
+              removeListener();
+              resolve(user);
+          },
+          reject
+      );
+  });
+};
+
+router.beforeEach(async(to, from, next)=>{
+  if(to.matched.some((record)=> record.meta.requiresAuth)){
+      if(await getCurrentUser()){
+          next();
+      }else {
+          console.log("You do not have any access!");
+          next("/login");
+      }
+  } else{
+      next();
+  }
+});
+
 export default router;
